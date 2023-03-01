31bf56a8766a6b3309157e961523eacfdb8c3a17
MOHANRAJK06 <mohanraj6596@gmail.com>
2023-03-01 12:28:12 +0530

    Giri

---
+++ src/org/bom/StringReverse.java
@@ -0,0 +1,46 @@
+package org.bom;
+
+import java.util.Iterator;
+
+public class StringReverse {
+	
+	
+	public static void main(String[] args) {
+		
+		String s = "My name is sam";
+		String b ="";
+		
+		
+    System.out.println("My:"+s);
+		
+		
+	
+		String[] d = s.split(" ");
+		
+	   
+		
+		for (int i=d.length-1; i>=0; i--) {
+			
+			
+		String x = d[i];
+			
+		
+		b = b+" "+x;
+		System.out.println(b);
+	}
+	System.out.println(b);
+}
+		
+	
+		
+		
+		
+			
+			
+		}
+	
+	
+	
+
+
+
